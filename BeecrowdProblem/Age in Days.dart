2f08db190febdd97b0c3c42e8e4d4f9817f27e90
maksudurrahmanmafi <maksudurrahmanmafi0@gmail.com>
2026-05-17 13:12:05 +0600

    Age in days

---
+++ BeecrowdProblem/Age in Days.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main(){
+  var a =int.parse(stdin.readLineSync()!);
+  var year = a~/365;
+  a%=365;
+  var month = a~/30;
+  a%=30;
+  print("${year} = ano(s)\n${month} = mes(es)\n${a} = dia(s)");
+}
